a197743fac856ae35e1d464ae23bb210165ec9ac
Leo Dn <leodoandev@gmail.com>
2023-10-11 08:14:46 -0500

    10/11

--- my-project/src/Components/Sidebar.js
+++ my-project/src/Components/Sidebar.js
@@ -3,15 +3,25 @@ import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import * as IoIcons from "react-icons/io";
 import * as RiIcons from "react-icons/ri";
+import { FaCalendar } from 'react-icons/fa'
+
+
  
 export const Sidebar = [
   {
-    title: "About Us",
-    path: "/about-us",
+    title:"Home",
+    path: "/",
     icon: <AiIcons.AiFillHome />,
+  }
+  ,
+  {
+    
+    title: "Menus",
+    path: "/about-us",
+    icon: <IoIcons.IoIosPaper />,
     iconClosed: <RiIcons.RiArrowDownSFill />,
     iconOpened: <RiIcons.RiArrowUpSFill />,
- 
+
     subNav: [
       {
         title: "Our Aim",
@@ -26,61 +36,19 @@ export const Sidebar = [
     ],
   },
   {
-    title: "Services",
-    path: "/services",
+    title: "News",
+    path: "/news",
     icon: <IoIcons.IoIosPaper />,
-    iconClosed: <RiIcons.RiArrowDownSFill />,
-    iconOpened: <RiIcons.RiArrowUpSFill />,
- 
-    subNav: [
-      {
-        title: "Service 1",
-        path: "/services/services1",
-        icon: <IoIcons.IoIosPaper />,
-        cName: "sub-nav",
-      },
-      {
-        title: "Service 2",
-        path: "/services/services2",
-        icon: <IoIcons.IoIosPaper />,
-        cName: "sub-nav",
-      },
-      {
-        title: "Service 3",
-        path: "/services/services3",
-        icon: <IoIcons.IoIosPaper />,
-      },
-    ],
   },
   {
     title: "Contact",
     path: "/contact",
     icon: <FaIcons.FaPhone />,
   },
+  
   {
-    title: "Events",
-    path: "/events",
-    icon: <FaIcons.FaEnvelopeOpenText />,
- 
-    iconClosed: <RiIcons.RiArrowDownSFill />,
-    iconOpened: <RiIcons.RiArrowUpSFill />,
- 
-    subNav: [
-      {
-        title: "Event 1",
-        path: "/events/events1",
-        icon: <IoIcons.IoIosPaper />,
-      },
-      {
-        title: "Event 2",
-        path: "/events/events2",
-        icon: <IoIcons.IoIosPaper />,
-      },
-    ],
-  },
-  {
-    title: "Support",
-    path: "/support",
-    icon: <IoIcons.IoMdHelpCircle />,
+    title: "Sign In",
+    path: "/signin",
+    icon: <FaCalendar/>,
   },
 ];
